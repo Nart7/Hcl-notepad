69ad09da2623f59a31fa705c6a1b1defaa8d7211
Nart7 <nart7@hotmail.co.uk>
2022-03-17 22:42:44 +0500

    add a new description

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,23 @@ const Home = () => {
           <div className={styles.head}>
             <Animation />
             <h1 className={styles.headline}>HCI Notepad</h1>
+            <br></br>
+            <p style={{
+              position: 'absolute',
+              top: '30%',
+              width: '30%',
+              color: 'black',
+            }}>
+              <strong
+                style={{
+                  color: 'black',
+                }}>
+                Description
+              </strong> :
+              <br></br>
+              the world's third largest country in size and nearly the third largest in terms of population.
+              Located in North America.
+            </p>
             <Draggable />
           </div>
         </div>
